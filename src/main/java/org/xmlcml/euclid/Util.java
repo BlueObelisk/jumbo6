125c58fc430c475cde2c92663430e55e133a7769
petermr <733515+petermr@users.noreply.github.com>
2008-07-03 09:40:49 +0000

    added ISO controls stuff

--- src/main/java/org/xmlcml/euclid/Util.java
+++ src/main/java/org/xmlcml/euclid/Util.java
@@ -1825,8 +1825,7 @@ public class Util implements EuclidConstants {
 	/**
 	 * remove all control (non-printing) characters
 	 * 
-	 * @param s
-	 *            Description of the Parameter
+	 * @param s Description of the Parameter
 	 * @return Description of the Return Value
 	 */
 	public static String stripISOControls(String s) {
@@ -1844,6 +1843,75 @@ public class Util implements EuclidConstants {
 		return sb.toString();
 	}
 
+	/**
+	 * remove all control (non-printing) characters
+	 * 
+	 * @param s Description of the Parameter
+	 * @return Description of the Return Value
+	 */
+	public static String replaceISOControlsByMnemonic(String s) {
+		if (s == null) {
+			return null;
+		}
+		StringBuffer sb = new StringBuffer();
+		int l = s.length();
+		for (int i = 0; i < l; i++) {
+			char ch = s.charAt(i);
+			if (!Character.isISOControl(ch)) {
+				sb.append(translateToMnemonic(ch));
+			}
+		}
+		return sb.toString();
+	}
+	
+	/** translate non-printing character to ISO mnemonic.
+	 * e.g. 1 => SOH
+	 * @param ch [0 - 31] or 127
+	 * @return translation or empty string if out of range
+	 */
+	public static String translateToMnemonic(char ch) {
+		switch (ch) {
+		
+			case 0: return "NUL"; 
+			case 1: return "SOH"; 
+			case 2: return "STX"; 
+			case 3: return "ETX"; 
+			case 4: return "EOT"; 
+			case 5: return "ENQ"; 
+			case 6: return "ACK"; 
+			case 7: return "BEL"; 
+			case 8: return "BS"; 
+			case 9: return "HT"; 
+			case 10: return "LF"; 
+			case 11: return "VT"; 
+			case 12: return "FF"; 
+			case 13: return "CR"; 
+			case 14: return "SO"; 
+			case 15: return "SI"; 
+			case 16: return "DLE"; 
+			case 17: return "DC1"; 
+			case 18: return "DC2"; 
+			case 19: return "DC3"; 
+			case 20: return "DC4"; 
+			case 21: return "NAK"; 
+			case 22: return "SYN"; 
+			case 23: return "ETB"; 
+			case 24: return "CAN"; 
+			case 25: return "EM"; 
+			case 26: return "SUB"; 
+			case 27: return "ESC"; 
+			case 28: return "FS"; 
+			case 29: return "GS"; 
+			case 30: return "RS"; 
+			case 31: return "US"; 
+			case 127: return "DEL"; 
+			default: return ""; 
+			
+		}
+		
+		}
+		
+		
 	/** convert 2 UTF8 characters to single IsoLatin1 character.
 	 * quick and dirty
 	 * UTF8 C2 80 => 80 (etc)
