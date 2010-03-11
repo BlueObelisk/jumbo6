bcc6e0ff9bf6998a0ffd663fb2d28c778d79b656
dmj30 <dmj30@localhost>
2010-03-11 14:25:52 +0000

    Forgot to rename a method.

--- src/test/java/org/xmlcml/cml/tools/SpectrumToolTest.java
+++ src/test/java/org/xmlcml/cml/tools/SpectrumToolTest.java
@@ -12,31 +12,31 @@ import org.xmlcml.cml.element.CMLSpectrum;
 public class SpectrumToolTest {
 
 	@Test
-	public void cbeckProtonNmr() throws Exception {
+	public void checkIntegralAgainstProtonCount() throws Exception {
 		InputStream spectrumStream = ClassLoader.getSystemResourceAsStream("org/xmlcml/cml/tools/spectrum1.xml");
 		CMLSpectrum spectrum = (CMLSpectrum) new CMLBuilder().build(spectrumStream).getRootElement();
 		InputStream mol1Stream = ClassLoader.getSystemResourceAsStream("org/xmlcml/cml/tools/molecule1.xml");
 		CMLMolecule mol1 = (CMLMolecule) new CMLBuilder().build(mol1Stream).getRootElement();
-		assertFalse(SpectrumTool.getOrCreateTool(spectrum).checkProtonNmr(mol1));
+		assertFalse(SpectrumTool.getOrCreateTool(spectrum).checkIntegralAgainstProtonCount(mol1));
 		
 		InputStream mol2Stream = ClassLoader.getSystemResourceAsStream("org/xmlcml/cml/tools/spectrum1mol.xml");
 		CMLMolecule mol2 = (CMLMolecule) new CMLBuilder().build(mol2Stream).getRootElement().getFirstChildElement(CMLMolecule.TAG, CMLMolecule.CML_NS);
-		assertTrue(SpectrumTool.getOrCreateTool(spectrum).checkProtonNmr(mol2));
+		assertTrue(SpectrumTool.getOrCreateTool(spectrum).checkIntegralAgainstProtonCount(mol2));
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void checkProtonNmrNullMolecule() throws Exception {
+	public void checkIntegralAgainstProtonCountNullMolecule() throws Exception {
 		InputStream spectrumStream = ClassLoader.getSystemResourceAsStream("org/xmlcml/cml/tools/spectrum1.xml");
 		CMLSpectrum spectrum = (CMLSpectrum) new CMLBuilder().build(spectrumStream).getRootElement();
 		CMLMolecule mol = null;
-		SpectrumTool.getOrCreateTool(spectrum).checkProtonNmr(mol);
+		SpectrumTool.getOrCreateTool(spectrum).checkIntegralAgainstProtonCount(mol);
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void checkProtonNmrNoAtoms() throws Exception {
+	public void checkIntegralAgainstProtonCountNoAtoms() throws Exception {
 		InputStream spectrumStream = ClassLoader.getSystemResourceAsStream("org/xmlcml/cml/tools/spectrum1.xml");
 		CMLSpectrum spectrum = (CMLSpectrum) new CMLBuilder().build(spectrumStream).getRootElement();
 		CMLMolecule mol = new CMLMolecule();
-		SpectrumTool.getOrCreateTool(spectrum).checkProtonNmr(mol);
+		SpectrumTool.getOrCreateTool(spectrum).checkIntegralAgainstProtonCount(mol);
 	}
 }
